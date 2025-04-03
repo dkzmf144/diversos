e189f8000d03a20ad8660d41ed4e8528193bf87a
dkzmf144 <dkzmf144@gmail.com>
2025-04-03 15:44:37 -0300

    update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
   menu.classList.toggle("hidden");
   menu.classList.toggle("visible");
 });
+
+// Fechar o menu ao clicar fora dele
+document.addEventListener("click", function (event) {
+  const menu = document.getElementById("menu");
+  const toggleButton = document.getElementById("menu-toggle");
+
+  // Verifica se o clique foi fora do menu e do botão
+  if (!menu.contains(event.target) && !toggleButton.contains(event.target)) {
+    menu.classList.add("hidden");
+    menu.classList.remove("visible");
+  }
+});
