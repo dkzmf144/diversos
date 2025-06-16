1c01fff12aef88a0d50aa97acb28888f6d1bc7e5
dkzmf144 <dkzmf144@gmail.com>
2025-06-16 16:41:07 -0300

    atualizaçao bebidas

---
+++ Isaque/java/bebidascigarros.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
